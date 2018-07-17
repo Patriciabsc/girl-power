c01f1dfa923218e5b86b4821778f5f00fe4c71e7
Louise Darche <louise.darche@gmail.com>
2018-07-17 18:57:57 +0200

    created the database again

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_07_17_155255) do
+ActiveRecord::Schema.define(version: 2018_07_17_165047) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
