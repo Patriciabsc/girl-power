8bd58e7a1debd8463d29f7ff5861a79012b6f380
Louise Darche <louise.darche@gmail.com>
2018-07-20 18:44:54 +0200

    members model ok, need front

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,7 +5,11 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :trackable, :validatable
 
    SKILLS = ["marketing", "gestion de projet", "communication", "branding", "développement fullstack (Ruby on Rails, J ES6, CSS, HTML5)",
-    "développement de MVP", "développement de sites responsives", "référencement et SEO", "marketing digital", 
+    "développement de MVP", "développement de sites responsives", "référencement et SEO", "marketing digital",
     "Business development", "financement", "Accompagnement stratégique"]
+
+  def full_name
+    return "#{self.first_name.capitalize} #{self.last_name.capitalize}"
+  end
 
 end
